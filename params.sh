9c6ea222d5e375f01a7cdd0ed2f10d0c58fdb9b2
harsh upreti <harshupreti@harshs-air.lan>
2019-12-13 12:37:58 -0800

    Test and params

---
+++ params.sh
@@ -0,0 +1,3 @@
+export APCA_API_KEY_ID='PK8TJB4I5YZ4IRO6VVM8' &&
+export APCA_API_SECRET_KEY='pzJIbG8itMk1KNMAYYgZ/ZVa4IogpHIMoKTzFB7r' &&
+export APCA_API_BASE_URL=https://paper-api.alpaca.markets
---
+++ test.py
@@ -0,0 +1,43 @@
+import backtrader as bt
+import alpaca_backtrader_api
+import pandas as pd
+
+class SmaCross(bt.SignalStrategy):
+    def __init__(self):
+        sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
+        crossover = bt.ind.CrossOver(sma1, sma2)
+        self.signal_add(bt.SIGNAL_LONG, crossover)
+
+is_live = True
+
+cerebro = bt.Cerebro()
+cerebro.addstrategy(SmaCross)
+
+store = alpaca_backtrader_api.AlpacaStore()
+
+if is_live:
+    broker = store.getbroker()  # or just alpaca_backtrader_api.AlpacaBroker()
+    cerebro.setbroker(broker)
+else:
+    cerebro.broker.setcash(100000)
+    cerebro.broker.setcommission(commission=0.0)
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=20)
+
+DataFactory = store.getdata # or use alpaca_backtrader_api.AlpacaData
+if is_live:
+    data0 = DataFactory(
+        dataname='AAPL',
+        timeframe=bt.TimeFrame.TFrame("Minutes"),
+        )
+else:
+    data0 = DataFactory(
+        dataname='AAPL',
+        timeframe=bt.TimeFrame.TFrame("Minutes"),
+        fromdate=pd.Timestamp('2018-11-15'),
+        todate=pd.Timestamp('2018-11-17'),
+        historical=True)
+cerebro.adddata(data0)
+
+cerebro.run()
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+cerebro.plot()
