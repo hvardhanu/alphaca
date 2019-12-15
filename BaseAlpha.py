819bbce38a95a97f62a60ffc1678d03c964d0ac5
harsh upreti <harshupreti@harshs-air.lan>
2019-12-14 22:16:42 -0800

    updates

---
+++ BaseAlpha.py
@@ -0,0 +1,63 @@
+import numpy as numpy
+import pandas as pd
+from pandas.tseries.offsets import DateOffset
+import datetime
+import alpaca_trade_api as tradeapi
+
+
+class BaseAlpha:
+    def __init__(self):
+        api = tradeapi.REST(key_id='PK8TJB4I5YZ4IRO6VVM8',
+                    secret_key='pzJIbG8itMk1KNMAYYgZ/ZVa4IogpHIMoKTzFB7r',
+                    base_url='https://paper-api.alpaca.markets',
+                    api_version='v2')  # or use ENV Vars shown below
+        self.api = api
+
+    def cancel_all_ord(self):
+        try:
+            self.api.cancel_all_orders()
+            print("** Canceling all orders **")
+        except  tradeapi.rest.APIError as e:
+            print("Error in cancel_all_ord - ", e)
+
+    def place_order_market(self, symbol, quantity):
+        try:
+            self.api.submit_order(symbol=symbol,
+                                  qty=quantity, side='buy',
+                                  time_in_force='day',
+                                  type='market')
+            
+            print("Placed order ", symbol," ", quantity)
+        except tradeapi.rest.APIError as e:
+            print("Error in place_order_market - ", e)
+    
+    def close_position(self,symbol):
+        try:
+            self.api.close_position(symbol=symbol)
+            print("Closing position for ",symbol)
+        except tradeapi.rest.APIError as e:
+            print(e)
+            print("Error ocurred while closing position, maybe no open position exist?")
+
+    def get_buying_power(self):
+        account = self.api.get_account()
+        return account.buying_power
+
+    def get_avg_price(self, days=0, symbol=''):
+
+        # TODO Implement market clock
+        # Start date is current date and end date is 'days' before start date
+        end_date = pd.Timestamp(datetime.date.today())
+        start_date = end_date-DateOffset(days=days)
+        end_date_iso = end_date.isoformat()
+        start_date_iso = start_date.isoformat()
+        try:
+            bars = self.api.get_barset(symbol, timeframe='day', start=start_date_iso,
+                                       end=end_date_iso, limit=days)
+        except tradeapi.rest.APIError as e:
+            print("Error in get_avg_price while getting bars- ", e)
+        # print(bars)
+        closed_end = bars[symbol][-1].c
+        closed_start = bars[symbol][0].c
+        perc_change = (closed_end-closed_start)/closed_start
+        return perc_change
--- alpha.py
+++ alpha.py
@@ -1,69 +1,38 @@
 
-import alpaca_trade_api as tradeapi
 from pandas.tseries.offsets import DateOffset
 import datetime
 import pandas as pd
-
-api = tradeapi.REST(key_id='PK8TJB4I5YZ4IRO6VVM8', secret_key='pzJIbG8itMk1KNMAYYgZ/ZVa4IogpHIMoKTzFB7r',base_url='https://paper-api.alpaca.markets', api_version='v2') # or use ENV Vars shown below
-
-def get_avg_price(days=3,symbol='MSFT'):
-    
-    #TODO Implement market clock
-    #Start date is current date and end date is 'days' before start date
-    end_date = pd.Timestamp(datetime.date.today())
-    start_date = end_date-DateOffset(days=days)
-    end_date_iso = end_date.isoformat()
-    start_date_iso = start_date.isoformat()
-    bars = api.get_barset(symbol,timeframe='day',start=start_date_iso,
-                        end=end_date_iso,limit=days)
-    #print(bars)
-    closed_end = bars[symbol][-1].c
-    closed_start = bars[symbol][0].c
-    perc_change = (closed_end-closed_start)/closed_start
-    return perc_change
+import BaseAlpha as BaseAlpha
 
 def main_func():
-     perc_change = get_avg_price(4,'MSFT')
-     strategy('MSFT',perc_change)
-    
-def strategy(symbol="",perc_change=0,isTest=False):    
-    account = api.get_account()
-    buying_power = account.buying_power
-    print(account.portfolio_value)
+    base = BaseAlpha.BaseAlpha()
+    perc_change = base.get_avg_price(4, 'MSFT')
+    strategy(base,'MSFT', perc_change, base)
 
-    #Get positions
-    print(api.list_positions())
-    order_type=""
-    print(perc_change)
+
+def strategy(base, symbol="", perc_change=0, isTest=False):
+    
+    buying_power = base.get_buying_power()
+    order_type = ""
+    print("Percentage Change:",perc_change)
 
     if perc_change < -0.003:
         if isTest:
             print("Seems like a unit test is running")
-            bar = api.get_barset(symbol,timeframe='day',limit=1)
+            bar = base.api.get_barset(symbol, timeframe='day', limit=1)
         else:
-            bar = api.get_barset(symbol,timeframe='min',limit=1)
+            bar = base.api.get_barset(symbol, timeframe='min', limit=1)
 
         price = bar[symbol][0].c
-        #print(type(buying_power))
-        #print(type(price))
         quantity = float(buying_power)/float(price)
         quantity = int(quantity)
-        print("buying_power ",buying_power)
-        print("Number of stocks bought ",quantity)
-        order_type="BOUGHT"
-        api.submit_order(symbol=symbol,qty=quantity, side='buy',time_in_force='day',type='market')
+        print("buying_power ", buying_power)
+        order_type = "BOUGHT"
+        base.place_order_market(symbol,quantity)
     elif perc_change > 0.011:
-        try:
-            api.close_position(symbol=symbol)
-
-            order_type="SOLD"
-        except Exception as e:
-            print(e)
-            print("Error ocurred while closing position, maybe no open position exist?")
+        base.close_position(symbol)
+        order_type = "SOLD"
     return order_type
 
-def cancelAllOrders():
-    api.cancel_all_orders()
-
 if __name__ == '__main__':
-     main_func()
+    main_func()
