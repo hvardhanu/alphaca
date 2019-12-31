23815b19d858062a4b3d8c3c857bd9de4033c2b8
harsh upreti <harshupreti@harshs-air.lan>
2019-12-31 11:25:10 -0800

    Adding better tests with MOCKS!

--- tests.py
+++ tests.py
@@ -1,25 +1,57 @@
-import alpha as alpha
+import alpaca_trade_api as tradeapi
+import BaseAlpha
 import unittest
-
-
+from unittest.mock import Mock,patch
 
 class AlphaTest(unittest.TestCase):
     def setUp(self): 
         pass
-
-    def test_get_avg_price(self):
-        self.assertEqual(alpha.get_avg_price(4,'MSFT'), -0.00382207578253715)
+        
+    def test_closePositionStock(self):
+        with patch('BaseAlpha.tradeapi') as mock:
+            api = mock.return_value
+            api.close_position.return_value = {}
+            base = BaseAlpha.BaseAlpha()
+            base.api = api
+            base.closePositionStock('MSFT')
+            self.assertEqual(api.close_position.call_count,1)
     
-    def test_bought_strategy(self):
-        sample_return = -0.7
-        self.assertEqual(alpha.strategy('MSFT',sample_return,True),"BOUGHT")
-    
-    def test_sold_strategy(self):
-        sample_return = 0.7
-        self.assertEqual(alpha.strategy('MSFT',sample_return, True),"SOLD")
+    def test_placeBuyWithStop(self):
+        with patch('BaseAlpha.tradeapi') as mock:
+            #Set up mock API
+            api = mock.return_value
+            base = BaseAlpha.BaseAlpha()
+            base.api = api
 
-    def tearDown(self):
-        alpha.cancelAllOrders()
+            #Set up some mock API Responses
+            mockbuyorder = Mock()
+            mockbuyorder.status = 'filled'
+            mockbuyorder.id = 1234
+            mockbuyorder.filled_qty = 2
+            mockbuyorder.filled_avg_price = 10
+            api.submit_order.return_value = mockbuyorder
+
+            mockstatusorder = Mock()
+            mockstatusorder.status = 'filled'
+            mockstatusorder.id = 1234
+            mockstatusorder.filled_qty = 2
+            mockstatusorder.filled_avg_price = 10
+            api.get_order.return_value = mockstatusorder
+            
+            #Test
+            self.assertEqual(base.placeBuyWithStop('MSFT',1,1),True)
+            self.assertEqual(api.submit_order.call_count,2)
+            api.submit_order.reset_mock()
+
+            mockstatusorder.status = 'pending'
+            self.assertEqual(base.placeBuyWithStop('MSFT',1,1),False)
+            self.assertEqual(api.submit_order.call_count,1)
+            api.submit_order.reset_mock()
+
+            #Make the API submit order function raise an exception
+            api.submit_order.side_effect = Exception("BOOM!")
+            self.assertEqual(base.placeBuyWithStop('MSFT',1,1),False)
+            self.assertEqual(api.submit_order.call_count,1)
 
 
 if __name__ == '__main__':
